2fb2d5f20e3ff43ca6d1f799fd0a15ed5a8154a0
XenorInspire <nicolas.fouchard94370@gmail.com>
2020-10-08 13:17:03 +0200

    ADD select menu for virtual disk format

--- deployVM.sh
+++ deployVM.sh
@@ -1,12 +1,11 @@
 # This script is used to quickly deploy a Virtual Machine in KVM
 # Created by XenorInspire
 
-# Check if default network interface is started with (in root user) :
+# Check is default is started with (in root user) :
 # virsh net-autostart --network default
 # virsh net-start --network default
 
 # Check the OS of you VM with : osinfo-query os
-# Replace line 35 (os variant)
 
 #!/bin/bash
 
@@ -29,7 +28,34 @@ read nameVM
 echo "Disk size (in GB)"
 read sizeDisk
 
+PS3="Which type of virtual disk you want to use ? "
+select rep in raw vmdk qcow2
+	do
+	if [ "$rep" = "raw" ]
+	then
+
+        formatVHD="raw"
+        break
+
+	fi
+	if [ "$rep" = "vmdk" ]
+	then
+
+        formatVHD="vmdk"
+        break
+
+	fi
+	if [ "$rep" = "qcow2" ]
+	then
+
+        formatVHD=qcow27
+        break
+
+	fi
+
+done
+
 path="../VirtualMachine_$nameVM"
 mkdir $path
 
-virt-install --name=$nameVM --vcpus=$cores --memory=$ram --cdrom=$nameISO --disk size=$sizeDisk,path=$path/$nameVM-disk.img,format=raw --os-type=Linux --os-variant=centos8
+virt-install --name=$nameVM --vcpus=$cores --memory=$ram --cdrom=$nameISO --disk size=$sizeDisk,path=$path/$nameVM-disk.img,format=$formatVHD --os-type=Linux --os-variant=centos8
